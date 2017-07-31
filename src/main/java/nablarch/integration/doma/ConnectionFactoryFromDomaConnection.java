65135c482bb14ddc336ecc1d61a9ed7100b7aa96
siosio <siosio12@gmail.com>
2017-07-31 10:12:07 +0900

    Nablarchのコネクション破棄前に保持しているStatementを開放するように
    NAB-174

--- src/main/java/nablarch/integration/doma/ConnectionFactoryFromDomaConnection.java
+++ src/main/java/nablarch/integration/doma/ConnectionFactoryFromDomaConnection.java
@@ -1,5 +1,7 @@
 package nablarch.integration.doma;
 
+import java.sql.Connection;
+
 import org.seasar.doma.internal.jdbc.util.JdbcUtil;
 
 import nablarch.core.db.connection.BasicDbConnection;
@@ -15,10 +17,34 @@ public class ConnectionFactoryFromDomaConnection extends ConnectionFactorySuppor
 
     @Override
     public TransactionManagerConnection getConnection(final String connectionName) {
-        final BasicDbConnection connection = new BasicDbConnection(
-                JdbcUtil.getConnection(DomaConfig.singleton()
-                                                 .getDataSource()));
+        final BasicDbConnection connection = new DomaBasedDbConnection(
+                JdbcUtil.getConnection(DomaConfig.singleton().getDataSource()));
         initConnection(connection, connectionName);
         return connection;
+    }
+
+    /**
+     * Domaが保持しているコネクションを使ってデータベースアクセスを実現するクラス。
+     */
+    private static class DomaBasedDbConnection extends BasicDbConnection {
+        /**
+         * 指定されたデータ接続を保持するオブジェクトを生成する。
+         *
+         * @param con データベース接続オブジェクト
+         */
+        public DomaBasedDbConnection(final Connection con) {
+            super(con);
+        }
+
+        /**
+         * 破棄処理を行う。
+         * 
+         * この実装では、保持しているステートメントの解放処理のみを行う。
+         * データベース接続の解放などは、Doma側で行う必要がある。
+         */
+        @Override
+        public void terminate() {
+            closeStatements();
+        }
     }
 }
